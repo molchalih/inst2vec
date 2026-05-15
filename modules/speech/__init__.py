72ab27044b7a01cf1c80e4756c82b797157da9cf
molchalih <26502678+molchalih@users.noreply.github.com>
2026-05-15 22:56:51 +0200

    refactor: split modules/speech.py into modules/speech/ package

--- modules/speech/__init__.py
+++ modules/speech/__init__.py
@@ -1,14 +1,6 @@
-"""Speech pipeline: classify (Whisper) + translate (TranslateGemma).
+"""Speech pipeline: classify (Whisper) + translate (TranslateGemma)."""
 
-Transitional shim: re-exports the legacy flat-module public API until
-classify.py / translate.py exist (Tasks 6 + 7 + 8).
-"""
-
-from importlib import import_module as _import_module
-
-_legacy = _import_module("modules._speech_flat")
-classify_speech = _legacy.classify_speech
-clean_speech = _legacy.clean_speech
-translate_speech = _legacy.translate_speech
+from modules.speech.classify import classify_speech, clean_speech
+from modules.speech.translate import translate_speech
 
 __all__ = ["classify_speech", "clean_speech", "translate_speech"]
